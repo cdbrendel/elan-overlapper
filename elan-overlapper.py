5b90ca1ed3cf00691c6678b0c359c24ed20022de
Chris Brendel <cdbrendel@gmail.com>
2016-10-30 20:51:52 -0700

    refactored into polymorphic structure

--- elan-overlapper.py
+++ elan-overlapper.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-import re
 from collections import defaultdict
 import argparse
 from sys import stderr
@@ -11,105 +10,113 @@ argParser.add_argument("-e", '--explicit-start', action="store_true", help="pars
 argParser.add_argument("-o", '--output-pattern', default="[INPUT-FILE]-indented[INPUT-EXTENSION]", help="filename format for the output indented files")
 args = argParser.parse_args()
 
-def scanStringForDigit(stringToScan):
-    digitString = ''
-    for character in stringToScan:
-        if character.isdigit():
-            digitString += character
+
+class ElanParser:
+    
+    def __init__(self, transcriptFile):
+        self.transcriptFile = transcriptFile
+        self.currentOverlapIndex = 0
+        self.linesSinceOverlap = 0
+        self.overlapItems = defaultdict(int)
+        self.newLines = []
+        self.additionalOffset = 0
+        self.newOverlapsInLine = 0
+
+    def GenerateIndentedText(self):
+        for linno, line in enumerate(inTranFile):
+            newLine = line
+            self.additionalOffset = 0
+            self.newOverlapsInLine = 0
+            lineContainedOverlap = False
+            for charpos, character in enumerate(line):
+                if character == '[':
+                    print('Found new overlap at line {} pos {}'.format(linno, charpos))
+                    newLine = self.handleOverlap(newLine, charpos+self.additionalOffset)
+                    lineContainedOverlap = True
+            if lineContainedOverlap:
+                self.linesSinceOverlap = 0
+            else:
+                self.linesSinceOverlap += 1
+            self.newLines.append(newLine)
+        return self.newLines
+
+    def ParseOutputFilenameTemplate(self, inTranFilename, outputPattern):
+        return outputPattern.replace('[INPUT-FILE]', os.path.splitext(inTranFilename)[0]).replace('[INPUT-EXTENSION]', os.path.splitext(inTranFilename)[1])       
+
+    def WriteNewFile(self, inTranFilename, outputPattern):
+        with open(self.ParseOutputFilenameTemplate(inTranFilename, outputPattern), 'w') as outTranFile:
+            for line in self.newLines:
+                outTranFile.write(self.cleanNewLine(line)) 
+
+    def scanStringForDigit(self, stringToScan):
+        digitString = ''
+        for character in stringToScan:
+            if character.isdigit():
+                digitString += character
+            else:
+                break
+        return digitString
+
+    def handleOverlap(self, line, charpos):
+        pass
+
+    def cleanNewLine(self, line):
+        pass
+
+
+class SimpleElanParser(ElanParser):
+    def handleOverlap(self, line, charpos):
+        print(self.linesSinceOverlap)
+        if self.linesSinceOverlap > 0:
+            print("Reseting overlaps...")
+            self.overlapItems = defaultdict(int)
+            self.linesSinceOverlap = 0
+        newLine = line
+        adjustedCharpos = charpos - 1 # I forget why this is necessary
+        self.currentOverlapIndex = self.scanStringForDigit(line[charpos + 1:]) or 0
+        print("Working on {} index".format(self.currentOverlapIndex))
+        if not self.overlapItems[self.currentOverlapIndex]:
+            # Assuming this is an entirely new block. Start a new reference position.
+            self.overlapItems[self.currentOverlapIndex] = adjustedCharpos
+            print('\tDoing new block. Next will start at char {}'.format(self.overlapItems[self.currentOverlapIndex]))
         else:
-            break
-    return digitString
+            newLine = newLine[:adjustedCharpos] + ' ' * (self.overlapItems[self.currentOverlapIndex] - (adjustedCharpos)) + newLine[adjustedCharpos:] # The -1 here accounts for the eventual removal of the double [[
+            print('\tAdding position offset {}'.format(self.overlapItems[self.currentOverlapIndex] - (adjustedCharpos)))
+            self.additionalOffset += (self.overlapItems[self.currentOverlapIndex] - (adjustedCharpos))
+        return newLine
+
+    def cleanNewLine(self, line):
+        return line
+
+
+class MarkedElanParser(ElanParser):
+    def handleOverlap(self, line, charpos):
+        newLine = line
+        adjustedCharpos = charpos - 1 # I forget why this is necessary
+        if line[charpos:charpos + 1] == '[[':
+            self.currentOverlapIndex = self.scanStringForDigit(line[charpos + 2:]) or 0
+            # This is an entirely new block. Start a new reference position.
+            self.overlapItems[self.currentOverlapIndex] = adjustedCharpos - self.newOverlapsInLine # This takes into account the additional offset caused by previous offset actions in this line (multiple overlaps per IU), caused by the eventual removal of the double [[]]
+            print('\tDoing new block. Next will start at char {}'.format(self.overlapItems[self.currentOverlapIndex]))
+            self.newOverlapsInLine += 1
+        else: 
+            self.currentOverlapIndex = self.scanStringForDigit(line[charpos + 1:]) or 0
+            newLine = newLine[:adjustedCharpos] + ' ' * (self.overlapItems[self.currentOverlapIndex] - (adjustedCharpos)) + newLine[adjustedCharpos:] # The -1 here accounts for the eventual removal of the double [[
+            print('\tAdding position offset {}'.format(self.overlapItems[self.currentOverlapIndex] - (adjustedCharpos)))
+            self.additionalOffset += (self.overlapItems[self.currentOverlapIndex] - (adjustedCharpos))
+        return newLine
+
+    def cleanNewLine(self, line):
+        return line.replace('[[', '[')
+
 
 if args.explicit_start:
-    for argFilename in args.input_file:
-        inTranFilename = argFilename
-        newLines = []
-        with open(inTranFilename, 'r') as inTranFile:
-            overlapItems = defaultdict(int)
-            linesSinceLast = defaultdict(int)
-            for linno, line in enumerate(inTranFile):
-                newLine = line
-                additionalOffset = 0
-                overlapsInLine = 0
-                for charpos, character in enumerate(line):
-                    if character == '[':
-                        if (line[charpos - 1] == '['): 
-                            # If the previous character was a [, the current [ just represents a new overlap; skip over it, it'll be removed later
-                            continue
-                        print('Found [ at line {} pos {}'.format(linno, charpos))
-                        if line[charpos + 1] == '[':
-                            # If this is a new overlap, the index character position is offset +2; otherwise, it's adjacent
-                            newOverlap = True
-                            indexCharacterPos = charpos + 2
-                        else:
-                            newOverlap = False
-                            indexCharacterPos = charpos + 1
-                        charpos -= 1
-                        if not line[indexCharacterPos].isdigit():
-                            # If there's no index character, assume it's 0 index
-                            digit = 0
-                            print('\tNo digit after [ ({}); assuming 0'.format(line[indexCharacterPos]))
-                        else:
-                            digit = scanStringForDigit(line[indexCharacterPos:])
-                            print('\t{} found after ['.format(digit))
-
-                        if newOverlap:
-                            # Assuming this is an entirely new block. Start a new reference position.
-                            overlapItems[digit] = additionalOffset + charpos - overlapsInLine # This takes into account the additional offset caused by previous offset actions in this line (multiple overlaps per IU)
-                            print('\tDoing new block. Next will start at char {}'.format(overlapItems[digit] ))
-                            overlapsInLine += 1
-                        else:
-                            newLine = newLine[:charpos] + ' ' * (overlapItems[digit] - (charpos)) + newLine[charpos:] # The -1 here accounts for the eventual removal of the double [[
-                            print('\tAdding position offset {}'.format(overlapItems[digit] - (charpos)))
-                            additionalOffset += (overlapItems[digit] - (charpos))
-                        linesSinceLast[digit] = linno
-                        
-                newLines.append(newLine)
-        with open(args.output_pattern.replace('[INPUT-FILE]', os.path.splitext(inTranFilename)[0]).replace('[INPUT-EXTENSION]', os.path.splitext(inTranFilename)[1]), 'w') as outTranFile:
-            for line in newLines:
-                outTranFile.write(line.replace('[[', '['))
+    elanParser = MarkedElanParser
 else:
-    for argFilename in args.input_file:
-        inTranFilename = argFilename
-        newLines = []
-        with open(inTranFilename, 'r') as inTranFile:
-            overlapItems = defaultdict(int)
-            linesSinceOverlap = 0
-            for linno, line in enumerate(inTranFile):
-                newLine = line
-                additionalOffset = 0
-                lineContainsOverlap = False
-                for charpos, character in enumerate(line):
-                    if character == '[':
-                        print('Found [ at line {} pos {}'.format(linno, charpos))
-                        indexCharacterPos = charpos + 1
-                        charpos -= 1
+    elanParser = SimpleElanParser
 
-                        if not line[indexCharacterPos].isdigit():
-                            # If there's no index character, assume it's 0 index
-                            digit = 0
-                            print('\tNo digit after [ ({}); assuming 0'.format(line[indexCharacterPos]))
-                        else:
-                            digit = scanStringForDigit(line[indexCharacterPos:])
-                            print('\t{} found after ['.format(digit))
-
-                        if not overlapItems[digit] or linesSinceOverlap > 0:
-                            # Assuming this is an entirely new block. Start a new reference position.
-                            overlapItems[digit] = additionalOffset + charpos
-                            print('\tDoing new block. Next will start at char {}'.format(overlapItems[digit] ))
-                        else:
-                            newLine = newLine[:charpos] + ' ' * (overlapItems[digit] - (charpos)) + newLine[charpos:] # The -1 here accounts for the eventual removal of the double [[
-                            print('\tAdding position offset {}'.format(overlapItems[digit] - (charpos)))
-                            additionalOffset += (overlapItems[digit] - (charpos))
-                        
-                        lineContainsOverlap = True
-                if not lineContainsOverlap:
-                    print("Reseting overlaps in line {}".format(linno))
-                    linesSinceOverlap += 1
-                    overlapItems = defaultdict(int)
-                else:
-                    linesSinceOverlap = 0
-                newLines.append(newLine)
-        with open(args.output_pattern.replace('[INPUT-FILE]', os.path.splitext(inTranFilename)[0]).replace('[INPUT-EXTENSION]', os.path.splitext(inTranFilename)[1]), 'w') as outTranFile:
-            for line in newLines:
-                outTranFile.write(line) 
+for argFilename in args.input_file:
+    with open(argFilename, 'r') as inTranFile:
+        currentElanParser = elanParser(inTranFile)
+        currentElanParser.GenerateIndentedText()
+        currentElanParser.WriteNewFile(argFilename, args.output_pattern)
