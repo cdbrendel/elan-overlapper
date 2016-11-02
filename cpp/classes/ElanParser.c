1cb8e7e379599fb34ab6f62a28446013aa32b619
= <cdbrendel@gmail.com>
2016-11-02 14:33:05 -0700

    removed non-master C++ files (only on development now)

--- cpp/classes/ElanParser.c
+++
@@ -1,5 +0,0 @@
-class ElanParser{
-    virtual int testFunction() {
-
-    }
-};
--- cpp/elan-overlapper.c
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "lib/optionparser.h"
-#include "classes/ElanParser.c"
-
-int main() {
-    std::cout << "Testing";
-}
